1a6331d804fd9abca1d7ffed18d4304108f755cc
dallano <atwood.dallan@hotmail.com>
2023-10-11 21:24:12 -0600

    Chocobo fix attempt
    Reverted zone changes

--- modules/era/sql/era_zone_settings.sql
+++ modules/era/sql/era_zone_settings.sql
@@ -24,6 +24,9 @@ UPDATE `zone_settings` SET `updatedmesh` = 1, `forcecarefulpathing` = 1 WHERE `z
 UPDATE `zone_settings` SET `updatedmesh` = 1, `forcecarefulpathing` = 1 WHERE `zoneid` = 139;
 UPDATE `zone_settings` SET `updatedmesh` = 1, `forcecarefulpathing` = 1 WHERE `zoneid` = 128;
 
+-- This will subtract the bitwise 4 from the misc column only if the bitwise exists
+UPDATE `zone_settings` SET `misc` = `misc` & ~4 WHERE `zoneid` IN (5, 7, 24, 25, 111, 112, 113, 122, 126, 127, 128) AND (`misc` & 4) <> 0;
+
 -- Dynamis Zones
 UPDATE `zone_settings` SET `misc` = `misc` | 256 WHERE `name` LIKE "Dynamis%" AND `name` NOT LIKE "Dynamis_%_[D]" AND ((`misc` & 256) = 0); -- Adds zonewide loot pool if it does not have it
 UPDATE `zone_settings` SET `misc` = `misc` & ~8 WHERE (name = "Dynamis-San_dOria" OR name = "Dynamis-Windurst" OR name = "Dynamis-Bastok" OR name = "Dynamis-Jeuno" OR name = "Dynamis-Tavnazia") AND (`misc` & 8) = 8;
