ee726895ef194110e3dc32f8d0bb6ed059128b24
dallano <atwood.dallan@hotmail.com>
2023-10-17 20:19:20 -0600

    Defender Adjustments
    Adjusts defender to not infinitely give def

--- scripts/globals/effects/defender.lua
+++ scripts/globals/effects/defender.lua
@@ -6,24 +6,33 @@ require("scripts/globals/jobpoints")
 local effectObject = {}
 
 effectObject.onEffectGain = function(target, effect)
-    local spxiPower = target:getSkillLevel(xi.skill.SHIELD) * 0.25
+    local power = 0
 
-    target:addMod(xi.mod.DEFP, 25 + spxiPower)
+    if target:isPC() then
+        power = target:getSkillLevel(xi.skill.SHIELD) * 0.25
+        target:setCharVar("[DEFENDER]spxiPower", power)
+    end
+
+    target:addMod(xi.mod.DEFP, 25 + power)
     target:addMod(xi.mod.ENMITY, 5)
-    target:addMod(xi.mod.RATTP, -25)
-    target:addMod(xi.mod.ATTP, -25)
+    target:addMod(xi.mod.RATTP, -25 - (power / 2))
+    target:addMod(xi.mod.ATTP, -25 - power / 2)
 end
 
 effectObject.onEffectTick = function(target, effect)
 end
 
 effectObject.onEffectLose = function(target, effect)
-    local spxiPower = target:getSkillLevel(xi.skill.SHIELD) * 0.25
+    local power = 0
 
-    target:delMod(xi.mod.DEFP, 25 + spxiPower)
+    if target:isPC() then
+        power = target:getCharVar("[DEFENDER]spxiPower")
+    end
+
+    target:delMod(xi.mod.DEFP, 25 + power)
     target:delMod(xi.mod.ENMITY, 5)
-    target:delMod(xi.mod.ATTP, -25 - (spxiPower / 2))
-    target:delMod(xi.mod.RATTP, -25 - (spxiPower / 2))
+    target:delMod(xi.mod.ATTP, -25 - (power / 2))
+    target:delMod(xi.mod.RATTP, -25 - (power / 2))
 end
 
 return effectObject
